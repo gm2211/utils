7799c6cca4c95a4d533c6cf07527db79c6f11bf8
Giulio Mecocci <gmecocci@palantir.com>
2017-05-03 11:37:42 -0700

    Duplicating nodes when adding, to prevent ghost substrings from showing up (e.g. 'abbc' yielding true for 'abc')

--- scala/src/main/scala/SuffixTree.scala
+++ scala/src/main/scala/SuffixTree.scala
@@ -1,25 +1,22 @@
-import java.util.UUID
+import java.util.concurrent.atomic.AtomicLong
 
 import scala.collection.mutable
 
 case class Node(
+  id: Long,
   char: Char,
   indices: mutable.LinkedHashSet[Int] = mutable.LinkedHashSet(),
-  children: mutable.HashMap[Char, UUID] = mutable.HashMap(),
-  id: UUID = UUID.randomUUID()
+  children: mutable.HashMap[Char, Long] = mutable.HashMap()
 )
 
 class SuffixTree {
-  val nodesById: mutable.HashMap[UUID, Node] = mutable.HashMap()
-  val rootIdsByChar: mutable.HashMap[Char, UUID] = mutable.HashMap()
-  var activeNodeIds: Set[UUID] = Set()
+  val curNodeId: AtomicLong = new AtomicLong(-1)
+  val nodesById: mutable.HashMap[Long, Node] = mutable.HashMap()
+  val rootIdsByChar: mutable.HashMap[Char, Long] = mutable.HashMap()
+  var activeNodeIds: Set[Long] = Set()
 
   def addChar(char: Char, idx: Int): SuffixTree = {
-    val node = Node(char, mutable.LinkedHashSet(idx))
-    val newActiveNodes = Set.newBuilder[UUID]
-
-    nodesById.put(node.id, node)
-    newActiveNodes += node.id
+    val newActiveNodes = Set.newBuilder[Long]
 
     activeNodeIds.foreach { activeNodeId =>
       val activeNode = nodesById(activeNodeId)
@@ -28,23 +25,35 @@ class SuffixTree {
         newActiveNodes += activeNode.children(char)
         nodesById(activeNode.children(char)).indices += idx
       } else {
-        activeNode.children.put(char, node.id)
-        newActiveNodes += node.id
+        val copy = storeNode(char, idx)
+        activeNode.children.put(char, copy.id)
+        newActiveNodes += copy.id
       }
     }
 
     rootIdsByChar.get(char) match {
-      case None => rootIdsByChar.put(char, node.id)
-      case Some(rootId) => newActiveNodes += rootId; nodesById(rootId).indices += idx
+      case None =>
+        val node = storeNode(char, idx)
+        rootIdsByChar.put(char, node.id)
+        newActiveNodes += node.id
+      case Some(rootId) =>
+        nodesById(rootId).indices += idx
+        newActiveNodes += rootId
     }
 
     activeNodeIds = newActiveNodes.result()
     this
   }
 
+  private def storeNode(char: Char, idx: Int): Node = {
+    val node = Node(curNodeId.incrementAndGet(), char, mutable.LinkedHashSet(idx))
+    nodesById.put(node.id, node)
+    node
+  }
+
   def longestPrefixLen(string: String): Option[(Int, List[Int])] = {
     var len = 0
-    var curNode = Node('$', children = rootIdsByChar)
+    var curNode = Node(-1, '$', children = rootIdsByChar)
 
     scala.util.control.Breaks.breakable {
       for (curChar <- string) {
@@ -62,14 +71,30 @@ class SuffixTree {
 
   def find(string: String): List[Int] = {
     longestPrefixLen(string) match {
-      case Some((len, indices)) if len == string.length => indices
+      case Some((len, indices)) if len == string.length => indices.map(_ + 1 - len)
       case _ => List()
     }
+  }
+
+  case class ResolvedNode(
+    id: Long,
+    char: Char,
+    children: Map[Char, ResolvedNode],
+    indices: mutable.LinkedHashSet[Int]
+  )
+  def resolve: (Long) => ResolvedNode = { id =>
+    val node: Node = nodesById(id)
+    ResolvedNode(node.id, node.char, node.children.mapValues(resolve).toMap, node.indices)
+  }
+
+  def resolved(): Map[Char, Product with Serializable with Object] = {
+    rootIdsByChar
+      .mapValues(resolve)
+      .toMap
   }
 }
 
 object SuffixTree {
-
   def build(string: String): SuffixTree = {
     string.zipWithIndex.foldLeft(new SuffixTree()) {
       case (tree, (char, idx)) => tree.addChar(char, idx)
