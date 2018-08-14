0af4568d288e60a0be15c2f99c62579788e3fd32
Giulio Mecocci <giuliomecocci@hotmail.it>
2018-08-14 07:13:04 -0400

    Re-structured project

---
+++ scala/src/main/scala/com/gm2211/debug/PrintExtensions.scala
@@ -0,0 +1,31 @@
+package com.gm2211.debug
+
+class PrintExtensions {
+  implicit class PrettyPrintable(obj: Any) {
+
+    def prettyPrint(depth: Int = 0): String = {
+      val stringBuilder = new StringBuffer()
+      val tabs = "\t" * depth
+
+      obj match {
+        case map: Map[_, _] =>
+          stringBuilder.append(s"{\n")
+          stringBuilder.append(
+            map.map { case (key, value) => s"$tabs\t$key -> ${value.prettyPrint(depth + 1)}" }.mkString(s",\n")
+          )
+          stringBuilder.append(s"\n$tabs}")
+
+        case list: List[_] =>
+          stringBuilder.append(s"[\n")
+          stringBuilder.append(
+            list.map(value => s"$tabs\t${value.prettyPrint(depth + 1)}").mkString(s",\n")
+          )
+          stringBuilder.append(s"\n$tabs]")
+        case _ =>
+          stringBuilder.append(s"$obj")
+      }
+
+      stringBuilder.toString
+    }
+  }
+}
